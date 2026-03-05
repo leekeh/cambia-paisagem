8aa9593dcc25bb5c860ccb3ac36da69315fad464
Lieke Beunders <71131586+leekeh@users.noreply.github.com>
2026-03-05 21:59:55 +0100

    feat: make english the default

--- astro.config.mjs
+++ astro.config.mjs
@@ -24,7 +24,7 @@ export default defineConfig({
     },
   },
   i18n: {
-    defaultLocale: "pt",
+    defaultLocale: "en",
     locales: ["pt", "en", "de"],
     routing: {
       prefixDefaultLocale: true,
